807b3f45113f8b85902c51961f1946229742d783
alwynlombaard <alwyn@justgiving.com>
2014-07-31 13:19:14 +0100

    Generate widget embed code

--- generator.js
+++ generator.js
@@ -1,5 +1,23 @@
-var generatorApp = angular.module("generatorApp", []);
+var generatorApp = angular.module("generatorApp", []); 
 
-generatorApp.controller("AppCtrl", function ($scope){
-
+generatorApp.factory('guid', function() {
+  return {
+    generate: function(){
+		var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			return v.toString(16);
+		});     
+		return guid;
+	}
+  };
 });
+
+generatorApp.controller("AppCtrl", function ($scope, $window, guid){
+	$scope.jgAccount = "";
+	$scope.jgAccount64 = function(){return $window.btoa($scope.jgAccount);};
+	$scope.guid = guid.generate();
+});
+
+
+
+
